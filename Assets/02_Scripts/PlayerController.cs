83b1fa3b2df98c4dee091eec33cdc3c66e9cd78f
Lee JaeHyun <myleje@gmail.com>
2024-06-25 16:19:16 +0900

    .

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -26,6 +26,17 @@ public class PlayerController : MonoBehaviour
         //transform.position = transform.position + new Vector3(0, 0, 0.01f);
     }
 
+    /*
+        Vector3 ShortHand
+
+        Vector3.forward = Vector3(0, 0, 1)
+        Vector3.up      = Vector3(0, 1, 0)
+        Vector3.right   = Vector3(1, 0, 0)
+
+        Vector3.one     = Vector3(1, 1, 1)
+        Vector3.zero    = Vector3(0, 0, 0)    
+    */
+
 }
 
 /*
