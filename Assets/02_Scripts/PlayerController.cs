48eb25974c8f314c5ac31ff003c77e987ed98b24
Lee JaeHyun <myleje@gmail.com>
2024-06-25 15:33:19 +0900

    .\

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Keyboard 입력값
+    // 1. InputManager (Legacy)
+    // 2. InputSystem  (New)
+
+    // Up/Down Arrow 키값을 저장하기 위한 변수를 선언
+    public float v; // 상하 화살표 키값 저장할 변수
+    public float h; // 좌우 화살표 키값 저장할 변수
+
     void Start()
     {
-
+        Debug.Log("Hello World");
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        v = Input.GetAxis("Vertical");
     }
+
 }
