b75e4bb94a5bc2df3235a5797a2f9ee1db8587b1
Lee JaeHyun <myleje@gmail.com>
2024-07-01 16:19:00 +0900

    .

---
+++ Assets/02_Scripts/Bullet.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+    }
+}
