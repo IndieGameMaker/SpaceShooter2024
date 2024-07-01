5e35df553ed0981ead7db0da649f8b727fe7bc67
Lee JaeHyun <myleje@gmail.com>
2024-07-01 16:47:50 +0900

    .

--- Assets/02_Scripts/Bullet.cs
+++ Assets/02_Scripts/Bullet.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     private Rigidbody rb;
+    [SerializeField] private float speed = 1500.0f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.AddRelativeForce(Vector3.forward * 1500.0f);
+        rb.AddRelativeForce(Vector3.forward * speed);
     }
 }
---
+++ Assets/02_Scripts/WeaponController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
