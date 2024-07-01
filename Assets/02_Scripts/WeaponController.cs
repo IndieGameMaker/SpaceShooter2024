724f4f5b5ba5717b282a20671db3e27f8d88ddb5
Lee JaeHyun <myleje@gmail.com>
2024-07-01 16:52:26 +0900

    .

--- Assets/02_Scripts/WeaponController.cs
+++ Assets/02_Scripts/WeaponController.cs
@@ -5,15 +5,12 @@ public class WeaponController : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePos;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 동적으로 Bullet 생성
+            Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+        }
     }
 }
