2da27816707859ce4e92a535708f11cca4024aef
Lee JaeHyun <myleje@gmail.com>
2024-07-01 17:19:10 +0900

    .

--- Assets/02_Scripts/RemoveBullet.cs
+++ Assets/02_Scripts/RemoveBullet.cs
@@ -5,7 +5,7 @@ public class RemoveBullet : MonoBehaviour
     //충돌 콜백함수
     void OnCollisionEnter(Collision coll)
     {
-
+        Debug.Log(coll.gameObject.name);
     }
 }
 /*
@@ -21,6 +21,8 @@ public class RemoveBullet : MonoBehaviour
         OnTriggerStay       n
         OnTriggerExit       1
 
+    3. 충돌 콜백함수가 호출되는 필수조건
 
-
+        1. 양쪽 다 Collider 컴포넌트가 있어야 함.
+        2. 이동하는 게임오브젝트에는 Rigidbody 반드시 있어야 함.
 */
