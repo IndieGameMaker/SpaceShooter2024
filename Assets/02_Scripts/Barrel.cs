3ea329fbaf63d5d7f77fe40516fc34a2b50dc176
Lee JaeHyun <myleje@gmail.com>
2024-07-02 15:36:17 +0900

    .

--- Assets/02_Scripts/Barrel.cs
+++ Assets/02_Scripts/Barrel.cs
@@ -19,6 +19,12 @@ public class Barrel : MonoBehaviour
 
     private void ExpBarrel()
     {
-
+        var rb = this.gameObject.AddComponent<Rigidbody>();
+        // 폴발 효과 발생
+        rb.AddExplosionForce(
+                            1500.0f,            // 횡 폭발력
+                            transform.position, // 폭발 원점
+                            3.0f,               // 폭발 반경
+                            1800.0f);           // 종 폭발력
     }
 }
