d3c6a2e3ea2ad62bdfd313e8048461ce192e412b
Lee JaeHyun <myleje@gmail.com>
2024-07-02 15:42:25 +0900

    .

--- Assets/02_Scripts/Barrel.cs
+++ Assets/02_Scripts/Barrel.cs
@@ -1,6 +1,5 @@
-using System;
-using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Barrel : MonoBehaviour
 {
@@ -20,10 +19,13 @@ public class Barrel : MonoBehaviour
     private void ExpBarrel()
     {
         var rb = this.gameObject.AddComponent<Rigidbody>();
+
+        Vector3 impactPoint = Random.insideUnitSphere;
+
         // 폴발 효과 발생
         rb.AddExplosionForce(
                             1500.0f,            // 횡 폭발력
-                            transform.position, // 폭발 원점
+                            transform.position + impactPoint * 2.0f, // 폭발 원점
                             3.0f,               // 폭발 반경
                             1800.0f);           // 종 폭발력
     }
