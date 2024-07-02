428bfcb582b861a892e578f463bbb86899e43539
Lee JaeHyun <myleje@gmail.com>
2024-07-02 17:50:42 +0900

    \

--- Assets/02_Scripts/WeaponController.cs
+++ Assets/02_Scripts/WeaponController.cs
@@ -43,6 +43,11 @@ public class WeaponController : MonoBehaviour
         Vector2 offset = new Vector2(Random.Range(0, 2), Random.Range(0, 2)) * 0.5f;
         muzzleFlash.material.mainTextureOffset = offset;
 
+        // Scale 변경
+        float scale = Random.Range(1.0f, 2.5f);
+        muzzleFlash.transform.localScale = Vector3.one * scale;
+        //new Vector3(scale, scale, scale);
+
         muzzleFlash.enabled = true;
 
         yield return new WaitForSeconds(0.2f);
