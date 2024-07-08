ed600e34f1610966c029709ba25dda92e8304f5f
Lee JaeHyun <myleje@gmail.com>
2024-07-08 17:31:40 +0900

    .

--- Assets/02_Scripts/MonsterController.cs
+++ Assets/02_Scripts/MonsterController.cs
@@ -8,6 +8,14 @@ public class MonsterController : MonoBehaviour
     // 몬스터의 상태를 저장하는 변수
     public State state = State.IDLE;
 
+    // 공격사정거리
+    [SerializeField] private float attackDist = 2.0f;
+    // 추적사정거리
+    [SerializeField] private float traceDist = 10.0f;
+
+    // 주인공 캐릭터의 Transform 컴포넌트를 저장할 변수
+    private Transform playerTr;
+
     void Start()
     {
 
